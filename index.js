d8c4174100d9f11a4b41425b795acd3860da1afb
Benjamin Hallquist <benhall84707@gmail.com>
2019-04-02 08:49:36 -0400

    completed simple coin-toss app

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const port = 1337;
 const express = require('express');
 const app = express();
-const Fortune = require('./models/fortuneApp')
+const Handlers = require('./models/handlers');
+const {coinFlip} = Handlers
 
-app.get('/fortune', async (req, res)=>{
-    res.json(await Fortune.getRandom())
-})
+app.get('/coinFlip', coinFlip)
+
 app.listen(port, ()=>{
     console.log(`now listening on port: ${port}`)
 })
--- schema.sql
+++
@@ -1,5 +0,0 @@
-create table fortune_app (
-    id serial primary key,
-    fortunes text
-)
-;
